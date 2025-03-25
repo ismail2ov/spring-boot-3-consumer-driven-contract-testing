bda66a7134ae7f74139f322aef14312c30fcf398
Ismail Ahmedov <ismail2ov@gmail.com>
2025-03-25 04:36:45 +0100

    Implement contract at the consumer: Divide the price field to amount and currency

---
+++ shop/src/main/java/com/paradigmadigital/shop/domain/Price.java
@@ -0,0 +1,5 @@
+package com.paradigmadigital.shop.domain;
+
+public record Price(String amount, String currency) {
+
+}
--- shop/src/main/java/com/paradigmadigital/shop/domain/Product.java
+++ shop/src/main/java/com/paradigmadigital/shop/domain/Product.java
@@ -1,6 +1,6 @@
 package com.paradigmadigital.shop.domain;
 
-public record Product(Long id, String name, String price, Integer quantity) {
+public record Product(Long id, String name, Price price, Integer quantity) {
 
     public Product withQuantity(int quantity) {
         return new Product(this.id, this.name, this.price, quantity);
